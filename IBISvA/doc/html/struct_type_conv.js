bec9903fd1333ccb7ae528fa46e2f345f2bfbc05
dan157 <danwoods157@hotmail.com>
2021-10-27 21:30:12 -1000

    missing html files

---
+++ IBISvA/doc/html/struct_type_conv.js
@@ -0,0 +1,5 @@
+var struct_type_conv =
+[
+    [ "name", "struct_type_conv.html#aba3bf5c8b5a88796f922a486f08ea642", null ],
+    [ "value", "struct_type_conv.html#ae65b6c2bb4a8b8c36d81c0463c3a802f", null ]
+];
